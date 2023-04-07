00fb094e3175ca2524613566d2c954df9aa564b0
sakioo <53650208+thomaue@users.noreply.github.com>
2023-04-07 02:25:28 +0200

    Update test_pointeurs.c

--- test_pointeurs.c
+++ test_pointeurs.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void ft_switch(int nombre1, int nombre2)
+void ft_switch(int *nombre1, int *nombre2)
 {
     int temp = 0;
 
-    temp = nombre1;
-    nombre1 = nombre2;
-    nombre2 = temp;
+    temp = *nombre1;
+    *nombre1 = *nombre2;
+    *nombre2 = temp;
 }
 
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 
     printf("Avant fonction : NB 1 = %d, NB 2 = %d\n", nb1, nb2);
 
-    ft_switch(nb1, nb2); //fonction 
+    ft_switch(&nb1, &nb2); //fonction 
 
     printf("Après fonction : NB 1 = %d, NB 2 = %d\n", nb1, nb2);
     return 0;
