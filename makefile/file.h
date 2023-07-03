c9ede837c8179cd148b8d4e18c1d60ef3df95824
sakioo <53650208+thomaue@users.noreply.github.com>
2023-07-03 14:29:55 +0200

    planner

---
+++ makefile/file.h
@@ -0,0 +1,8 @@
+#ifndef __file__
+#define __file__
+
+
+void ft_putstr(char *str);
+
+
+#endif
---
+++ makefile/main.c
@@ -0,0 +1,11 @@
+#include "file.h"
+
+int main(void)
+{
+
+    char *lundi = "salut";
+
+    ft_putstr(lundi);
+
+    return 0;
+}
