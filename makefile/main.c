e767cc04123e2ec9d07c4937eb2d8dbf243ead74
sakioo <53650208+thomaue@users.noreply.github.com>
2023-07-05 09:58:54 +0200

    Update main.c

--- makefile/main.c
+++ makefile/main.c
@@ -4,23 +4,33 @@
 
 int main(void)
 {
-    int swit = 0;
-
-    char **lundi;
+    int *swit = 0;
     char *str;
+
+    int i = 0;
+
+    char **day = (char **)malloc(sizeof(char *) * 25);
+    while(i < 25)
+    {
+        day[i] = i;
+        i++;
+    }
+
+
+
 
     printf("welcome on planner\n\n");
 
-    while(swit < 3)
+    while(*swit < 3)
     {
         printf("0 : print | 1 : modifying | 2 : quit\n");
         scanf("%d", swit);
-        if(swit == 0)
+        if(*swit == 0)
         {
-            ft_show_tab(lundi);
+            // ft_show_tab(lundi);
         }
 
-        else if(swit == 1)
+        else if(*swit == 1)
         {
             printf("please enter the time and then the text to be inserted in this format :");
             printf("<hour> : <text>");
@@ -32,5 +42,7 @@ int main(void)
             break;
         }
     }
+
+    free(day);
     return 0;
 }
